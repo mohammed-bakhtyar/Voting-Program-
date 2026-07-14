44a463bd65ad1a192781bcf34acf6ce5861c8938
tomiokagiyuu123321 <mhamed.bakhtyar.cs@sulicihan.edu.krd>
2026-07-14 15:17:13 +0300

    style: change My Polls button to Home when on the index page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -273,12 +273,21 @@
                         Dashboard
                     </a>
                     @else
-                    <a href="{{ route('topics.index') }}" class="vp-btn vp-btn-ghost">
-                        <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"/>
-                        </svg>
-                        My Polls
-                    </a>
+                        @if(request()->routeIs('topics.index'))
+                            <a href="{{ route('home') }}" class="vp-btn vp-btn-ghost">
+                                <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18"/>
+                                </svg>
+                                Home
+                            </a>
+                        @else
+                            <a href="{{ route('topics.index') }}" class="vp-btn vp-btn-ghost">
+                                <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"/>
+                                </svg>
+                                My Polls
+                            </a>
+                        @endif
                     @endif
                     <form action="{{ route('logout') }}" method="POST" style="display:inline;margin:0;">
                         @csrf
